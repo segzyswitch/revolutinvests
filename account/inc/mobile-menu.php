a86cc8c19218db41f707865534ffc512464ea10a
segzyswitch <segzyswitch99@gmail.com>
2026-05-07 13:13:40 +0100

    remove chat

--- account/inc/mobile-menu.php
+++ account/inc/mobile-menu.php
@@ -49,17 +49,3 @@
     }
   });
 </script>
-
-
-<!-- Smartsupp Live Chat script -->
-<script type="text/javascript">
-var _smartsupp = _smartsupp || {};
-_smartsupp.key = '9e7226ea6fd4ef6bcb8d8a6927f6b28a6c5e376e';
-window.smartsupp||(function(d) {
-  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-  c.type='text/javascript';c.charset='utf-8';c.async=true;
-  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-})(document);
-</script>
-<noscript>Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a></noscript>
